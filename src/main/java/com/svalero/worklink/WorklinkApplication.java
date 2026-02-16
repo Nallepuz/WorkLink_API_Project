a4c4cff6e28645d6eb157f958dd4936be5f4e95b
Néstor Allepuz <nallepuznaya@gmail.com>
2026-02-16 10:18:36 +0100

    Initial commit

---
+++ src/main/java/com/svalero/worklink/WorklinkApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.worklink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorklinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorklinkApplication.class, args);
+	}
+
+}
