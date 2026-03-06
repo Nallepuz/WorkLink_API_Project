2f6fd1ebb4a3cafe253835969255349f525ab6d4
Néstor Allepuz <nallepuznaya@gmail.com>
2026-03-06 04:20:23 +0100

    Add UserRepository

---
+++ src/main/java/com/svalero/worklink/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.svalero.worklink.repository;
+
+import com.svalero.worklink.model.User;
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    List<User> findAll();
+    Optional<User> findById(Long id);
+    List<User> findByEmail(String email);
+    List<User> findByName(String name);
+    List<User> findByActive(boolean active);
+}
